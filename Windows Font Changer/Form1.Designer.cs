93063206a713eea9e88823d82625e4066b0309eb
Ruslan <42177346+alcortazzo@users.noreply.github.com>
2020-02-03 15:45:27 +0200

    Add files via upload

---
+++ Windows Font Changer/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿using System.Windows.Forms;
+
+namespace Windows_Font_Changer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnCust = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.AccessibleDescription = "";
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(12)))), ((int)(((byte)(12)))), ((int)(((byte)(12)))));
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Enabled = false;
+            this.textBox1.ForeColor = System.Drawing.Color.White;
+            this.textBox1.Location = new System.Drawing.Point(119, 142);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(212, 22);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "Tahoma";
+            this.textBox1.Visible = false;
+            this.textBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.textBox1_MouseClick);
+            // 
+            // btnCust
+            // 
+            this.btnCust.Enabled = false;
+            this.btnCust.FlatAppearance.BorderSize = 0;
+            this.btnCust.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCust.ForeColor = System.Drawing.Color.White;
+            this.btnCust.Location = new System.Drawing.Point(337, 142);
+            this.btnCust.Name = "btnCust";
+            this.btnCust.Size = new System.Drawing.Size(75, 23);
+            this.btnCust.TabIndex = 1;
+            this.btnCust.TabStop = false;
+            this.btnCust.Text = "[E]nter";
+            this.btnCust.UseVisualStyleBackColor = false;
+            this.btnCust.Visible = false;
+            this.btnCust.Click += new System.EventHandler(this.btnCust_Click);
+            // 
+            // label1
+            // 
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(115, 107);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(300, 22);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "what do you want to do";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btn2
+            // 
+            this.btn2.FlatAppearance.BorderSize = 0;
+            this.btn2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn2.ForeColor = System.Drawing.Color.White;
+            this.btn2.Location = new System.Drawing.Point(268, 142);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(145, 23);
+            this.btn2.TabIndex = 8;
+            this.btn2.TabStop = false;
+            this.btn2.Text = "[s]et default font";
+            this.btn2.UseVisualStyleBackColor = false;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn1
+            // 
+            this.btn1.FlatAppearance.BorderSize = 0;
+            this.btn1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn1.ForeColor = System.Drawing.Color.White;
+            this.btn1.Location = new System.Drawing.Point(117, 142);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(145, 23);
+            this.btn1.TabIndex = 9;
+            this.btn1.TabStop = false;
+            this.btn1.Text = "[s]et custom font";
+            this.btn1.UseVisualStyleBackColor = false;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.FlatAppearance.BorderSize = 0;
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExit.ForeColor = System.Drawing.Color.White;
+            this.btnExit.Location = new System.Drawing.Point(443, 272);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 10;
+            this.btnExit.TabStop = false;
+            this.btnExit.Text = "[E]xit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // label2
+            // 
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(115, 181);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(300, 22);
+            this.label2.TabIndex = 11;
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.ActiveLinkColor = System.Drawing.Color.White;
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("Consolas", 8F);
+            this.linkLabel1.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(56)))), ((int)(((byte)(56)))), ((int)(((byte)(56)))));
+            this.linkLabel1.Location = new System.Drawing.Point(12, 285);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(79, 13);
+            this.linkLabel1.TabIndex = 12;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = ">_alcortazzo";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            this.linkLabel1.MouseEnter += new System.EventHandler(this.linkLabel1_MouseEnter);
+            this.linkLabel1.MouseLeave += new System.EventHandler(this.linkLabel1_MouseLeave);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(12)))), ((int)(((byte)(12)))), ((int)(((byte)(12)))));
+            this.ClientSize = new System.Drawing.Size(526, 303);
+            this.ControlBox = false;
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCust);
+            this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("Consolas", 9F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Opacity = 0.97D;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Windows Font Changer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnCust;
+        private System.Windows.Forms.Label label1;
+        private Button btn2;
+        private Button btn1;
+        private Button btnExit;
+        private Label label2;
+        private LinkLabel linkLabel1;
+    }
+}
+
---
+++ Windows Font Changer/Form1.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Windows_Font_Changer
+{
+    public partial class Form1 : Form
+    {
+        RegistryKey localMachineKey = Registry.LocalMachine;
+        string RegWay1 = @"SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts";
+        string RegWay2 = @"SOFTWARE\Microsoft\Windows NT\CurrentVersion\FontSubstitutes";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void btnCust_Click(object sender, EventArgs e)
+        {
+            //RegWay1();
+            RegistryKey localMachineKey = Registry.LocalMachine.OpenSubKey(RegWay1, true);
+
+            localMachineKey.SetValue("Segoe UI (TrueType)", "");
+            localMachineKey.SetValue("Segoe UI Bold (TrueType)", "");
+            localMachineKey.SetValue("Segoe UI Bold Italic (TrueType)", "");
+            localMachineKey.SetValue("Segoe UI Italic (TrueType)", "");
+            localMachineKey.SetValue("Segoe UI Light (TrueType)", "");
+            localMachineKey.SetValue("Segoe UI Semibold (TrueType)", "");
+            localMachineKey.SetValue("Segoe UI Symbol (TrueType)", "");
+
+            RegistryKey localMachineKey2 = Registry.LocalMachine.OpenSubKey(RegWay2, true);
+
+            //localMachineKey2.SetValue("login", "admin");
+            localMachineKey2.SetValue("Segoe UI", textBox1.Text);
+
+            label2.Text = "restart your computer to complete install";
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            btn1.Enabled = false;
+            btn1.Visible = false;
+            btn2.Enabled = false;
+            btn2.Visible = false;
+            label1.ForeColor = Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(41)))), ((int)(((byte)(77)))));
+            label1.Text = "enter font name WITHOUT ERRORS";
+            btnCust.Enabled = true;
+            btnCust.Visible = true;
+            textBox1.Enabled = true;
+            textBox1.Visible = true;
+            textBox1.Visible = true;
+            label2.Text = "";
+        }
+
+        private void textBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            textBox1.Text = "";
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            RegistryKey localMachineKey = Registry.LocalMachine.OpenSubKey(RegWay1, true);
+
+            localMachineKey.SetValue("Segoe UI (TrueType)", "segoeui.ttf");
+            localMachineKey.SetValue("Segoe UI Black (TrueType)", "seguibl.ttf");
+            localMachineKey.SetValue("Segoe UI Black Italic (TrueType)", "seguibli.ttf");
+            localMachineKey.SetValue("Segoe UI Bold (TrueType)", "segoeuib.ttf");
+            localMachineKey.SetValue("Segoe UI Bold Italic (TrueType)", "segoeuiz.ttf");
+            localMachineKey.SetValue("Segoe UI Emoji (TrueType)", "seguiemj.ttf");
+            localMachineKey.SetValue("Segoe UI Historic (TrueType)", "seguihis.ttf");
+            localMachineKey.SetValue("Segoe UI Italic (TrueType)", "segoeuii.ttf");
+            localMachineKey.SetValue("Segoe UI Light (TrueType)", "segoeuil.ttf");
+            localMachineKey.SetValue("Segoe UI Light Italic (TrueType)", "seguili.ttf");
+            localMachineKey.SetValue("Segoe UI Semibold (TrueType)", "seguisb.ttf");
+            localMachineKey.SetValue("Segoe UI Semibold Italic (TrueType)", "seguisbi.ttf");
+            localMachineKey.SetValue("Segoe UI Semilight (TrueType)", "segoeuisl.ttf");
+            localMachineKey.SetValue("Segoe UI Semilight Italic (TrueType)", "seguisli.ttf");
+            localMachineKey.SetValue("Segoe UI Symbol (TrueType)", "seguisym.ttf");
+            localMachineKey.SetValue("Segoe MDL2 Assets (TrueType)", "segmdl2.ttf");
+            localMachineKey.SetValue("Segoe Print (TrueType)", "segoepr.ttf");
+            localMachineKey.SetValue("Segoe Print Bold (TrueType)", "segoeprb.ttf");
+            localMachineKey.SetValue("Segoe Script (TrueType)", "segoesc.ttf");
+            localMachineKey.SetValue("Segoe Script Bold (TrueType)", "segoescb.ttf");
+            label1.ForeColor = Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(212)))), ((int)(((byte)(25)))));
+            label1.Text = "default font has been installed";
+            label2.Text = "restart your computer to complete install";
+
+            RegistryKey localMachineKey2 = Registry.LocalMachine.OpenSubKey(RegWay2, true);
+
+            localMachineKey2.DeleteValue("Segoe UI");
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("https://github.com/alcortazzo");
+        }
+
+        private void linkLabel1_MouseEnter(object sender, EventArgs e)
+        {
+            linkLabel1.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+        }
+
+        private void linkLabel1_MouseLeave(object sender, EventArgs e)
+        {
+            linkLabel1.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(56)))), ((int)(((byte)(56)))), ((int)(((byte)(56)))));
+        }
+    }
+}
